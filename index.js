bad2d96ba52b64857ceb231eec914b67224b1e45
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-22 16:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ function exactMatch(driver,object1){
   return driver.filter(function(d){
     for (const key in object1){
       for(const keyd in d){
+        debugger;
         return d[keyd] === object1[key];
       }
     }
