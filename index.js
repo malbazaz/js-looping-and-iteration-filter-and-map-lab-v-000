abb772599f995d072df7c7ce4c37993b8ef1f051
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-22 15:39:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ return driver.filter(function(d){
 }
 
 function driverNamesWithRevenueOver(driver, revenue){
-  return driver.filter(function(d){
-    return d.revenue > revenue;
+  driver.filter(function(d){
+   d.revenue > revenue;
     }
   })
   return driver.map(function(d){
