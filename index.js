a3f648c6e90c0482fe6aa3ced2fe6761e7bb647a
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-22 16:40:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function exactMatch(driver,object1){
 }
 
 function exactMatchToList(driver, object1){
-driver.filter(function(d){
+return driver.filter(function(d){
     for(const key in object1){
       if(d[key] === object1[key]){
         debugger;
