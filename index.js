07e08e4d1cae39004fc5704ee26e26ae5463027e
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-22 16:04:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,9 @@ function driverNamesWithRevenueOver(driver, revenue){
 function exactMatch(driver,object1){
   return driver.filter(function(d){
     for (const key in object1){
-      debugger;
-      return object1[key] === d;
+      for(const keyd in d){
+        return object1[key] === d[keyd];
+      }
     }
   })
 }
